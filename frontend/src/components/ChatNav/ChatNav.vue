f72bbec0eb43feb9c5b78b5d482eb93bcc075898
RediYo <rediyo@qq.com>
2023-09-03 11:26:23 +0800

    commit changes

--- frontend/src/components/ChatNav/ChatNav.vue
+++ frontend/src/components/ChatNav/ChatNav.vue
@@ -302,7 +302,7 @@ const saveSetting = () => {
         <NFormItem path="" label="最新版本">
           <NTag type="info" size="small" round>{{ lastVersion }}</NTag>
         </NFormItem>
-        <!-- <NFormItem path="token" label="开源地址">
+        <NFormItem path="token" label="开源地址">
           <NButton text tag="a" href="https://github.com/Harry-zklcdc/go-proxy-bingai" target="_blank" type="success">Harry-zklcdc/go-proxy-bingai</NButton>
         </NFormItem>
         <NFormItem path="token" label="原作者">
@@ -310,7 +310,7 @@ const saveSetting = () => {
         </NFormItem>
         <NFormItem path="token" label="原开源地址">
           <NButton text tag="a" href="https://github.com/adams549659584/go-proxy-bingai" target="_blank" type="success">adams549659584/go-proxy-bingai</NButton>
-        </NFormItem> -->
+        </NFormItem>
       </NForm>
       <template #action>
         <NButton ghost size="large" @click="isShowSetAboutModal = false" type="info">确定</NButton>
